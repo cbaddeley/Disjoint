f29d0f03eb70ac2bc0fe6157c9894e9f3684fbe7
cbaddeley <corybaddeley@gmail.com>
2017-09-04 17:39:42 -0400

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,12 @@
 USE game_db;
 
-INSERT INTO Levels (level_name) VALUES ('First Level');
-INSERT INTO Levels (level_name) VALUES ('second Level');
-INSERT INTO Levels (level_name) VALUES ('third Level');
+INSERT INTO Levels (level_name) VALUES ('Level 1');
+INSERT INTO Levels (level_name) VALUES ('Level 2');
+INSERT INTO Levels (level_name) VALUES ('Level 3');
+INSERT INTO Levels (level_name) VALUES ('Level 4');
+INSERT INTO Levels (level_name) VALUES ('Level 5');
+INSERT INTO Levels (level_name) VALUES ('Level 6');
+INSERT INTO Levels (level_name) VALUES ('Level 7');
+INSERT INTO Levels (level_name) VALUES ('Level 8');
+INSERT INTO Levels (level_name) VALUES ('Level 9');
+INSERT INTO Levels (level_name) VALUES ('Level 10');
