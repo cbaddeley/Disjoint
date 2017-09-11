c224dfddd794d3974f7b66f0fc0da7a28cd8c8e4
cbaddeley <corybaddeley@gmail.com>
2017-09-11 18:56:21 -0400

    removed some unneccesary html files

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,10 +7,6 @@ INSERT INTO Levels (level_name) VALUES ('Level 4');
 INSERT INTO Levels (level_name) VALUES ('Level 5');
 INSERT INTO Levels (level_name) VALUES ('Level 6');
 INSERT INTO Levels (level_name) VALUES ('Level 7');
-INSERT INTO Levels (level_name) VALUES ('Level 8');
-INSERT INTO Levels (level_name) VALUES ('Level 9');
-INSERT INTO Levels (level_name) VALUES ('Level 10');
 
 INSERT INTO Shops (item_name, reputation, backpack, secret)
 VALUES ("Motivation", 10), ("Nightmare book", 5), ("Curly lip hair", 15), ("Solved Folder", 10, false, true);
- 
