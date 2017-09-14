50bd5943022dd682d616fc6a575ba3b32147ba63
Will H <wj_hilliard@yahoo.com>
2017-09-14 19:37:16 -0400

    CSS changes, attempt to add routes

--- models/itemModel.js
+++ models/itemModel.js
@@ -6,9 +6,34 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: [1, 15]
       }
-    }
-  }, {
+    },
+    reputation: {
+  type: DataTypes.INTEGER,
+  defaultValue: 30
+  },
+  backpack: {
+  type: DataTypes.BOOLEAN,
+  defaultValue: false
+  },
+  //backpack will only exist on shop object
+  //plan to change
+  secret: {
+  type: DataTypes.BOOLEAN,
+  defaultValue: false,
+  }
+},{
     timestamps: false
   });
-  return Item;
+ 
+//Associate with another table (or model)
+Item.associate = (models) => {
+ // We're saying that an Item should belong to a Backpack
+    // An item can't be created without a Backpack due to the foreign key constraint
+    Item.belongsTo(models.Backpack, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+   return Item;
 };
