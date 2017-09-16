c5ffd6011d2a04c2392065a2d6bc80a54ec7c00f
cbaddeley <corybaddeley@gmail.com>
2017-09-15 23:43:19 -0400

    asdf

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set("view engine", "handlebars");
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
